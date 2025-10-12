756d398257a65d97ee1a0c4c39d29a4a7f819ab4
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2025-10-12 22:16:00 +0100

    Implement method calls

--- src/main/java/com/github/edumaxsantos/jlox/Resolver.java
+++ src/main/java/com/github/edumaxsantos/jlox/Resolver.java
@@ -15,7 +15,7 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
     private enum FunctionType {
-        NONE, FUNCTION
+        NONE, METHOD, FUNCTION
     }
 
     private class VariableInfo {
@@ -129,6 +129,11 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     public Void visitClassStmt(Stmt.Class stmt) {
         declare(stmt.name);
         define(stmt.name);
+
+        for (Stmt.Function method: stmt.methods) {
+            FunctionType declaration = FunctionType.METHOD;
+            resolveFunction(method, declaration);
+        }
         return null;
     }
 
