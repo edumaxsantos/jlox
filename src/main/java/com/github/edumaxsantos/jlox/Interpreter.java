7a5aef9b9c596d6744e52c85d601d646696397bb
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2025-10-12 17:01:59 +0100

    Fix bug where it was not evaluating each argument

--- src/main/java/com/github/edumaxsantos/jlox/Interpreter.java
+++ src/main/java/com/github/edumaxsantos/jlox/Interpreter.java
@@ -131,7 +131,11 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     public Object visitCallExpr(Expr.Call expr) {
         Object callee = evaluate(expr.callee);
 
-        List<Object> arguments = new ArrayList<>(expr.arguments);
+        List<Object> arguments = new ArrayList<>();
+
+        for (Expr argument: expr.arguments) {
+            arguments.add(evaluate(argument));
+        }
 
         if (!(callee instanceof LoxCallable function)) {
             throw new RuntimeError(expr.paren, "Can only call functions and classes.");
