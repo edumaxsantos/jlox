668897f60223bc83ab29a568ee888e93ae484d8e
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2025-10-15 01:54:29 +0100

    Change how to handle System.exit

--- src/main/java/com/github/edumaxsantos/jlox/Lox.java
+++ src/main/java/com/github/edumaxsantos/jlox/Lox.java
@@ -13,27 +13,55 @@ public class Lox {
     boolean hadRuntimeError = false;
 
     public static Interpreter interpreter;
+    public static SystemExit systemExit = new RealSystemExit();
 
     public static void main(String[] args) throws IOException {
-        if (args.length > 1) {
-            System.out.println("Usage: jlox [script]");
-            System.exit(LoxStatus.INVALID_PARAMS_ERROR.getErrorCode());
+        if (args.length > 2) {
+            System.out.println("Usage: jlox [-c] [script/code]");
+            systemExit.exit(LoxStatus.INVALID_PARAMS_ERROR);
+            return;
         }
+
+        if (args.length == 2 && !args[0].equals("-c")) {
+            System.out.println("Usage: jlox -c [code]");
+            systemExit.exit(LoxStatus.INVALID_PARAMS_ERROR);
+            return;
+        }
+
         var lox = new Lox();
         interpreter = new Interpreter(lox);
-        if (args.length == 1) {
+        if (args.length == 2) {
+            runScript(args[1], lox);
+        } else if (args.length == 1) {
             runFile(args[0], lox);
         } else {
             runPrompt(lox);
         }
-        System.exit(0);
+    }
+
+    private static void runScript(String script, Lox lox) {
+        run(script, lox);
+        if (lox.hadError) {
+            systemExit.exit(LoxStatus.ERROR);
+            return;
+        }
+        if (lox.hadRuntimeError) {
+            systemExit.exit(LoxStatus.RUNTIME_ERROR);
+            return;
+        }
     }
 
     private static void runFile(String path, Lox lox) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()), lox);
-        if (lox.hadError) System.exit(LoxStatus.ERROR.getErrorCode());
-        if (lox.hadRuntimeError) System.exit(LoxStatus.RUNTIME_ERROR.getErrorCode());
+        if (lox.hadError) {
+            systemExit.exit(LoxStatus.ERROR);
+            return;
+        }
+        if (lox.hadRuntimeError) {
+            systemExit.exit(LoxStatus.RUNTIME_ERROR);
+            return;
+        }
     }
 
     private static void runPrompt(Lox lox) throws IOException {
